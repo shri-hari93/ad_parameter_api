f4eaf49d2ac93a57a6d28b826dec7db875f72d20
shri-hari93 <shrihari.g93@gmail.com>
2022-08-03 21:45:39 +0200

    better styling spec

--- app/serializers/placement_serializer.rb
+++ app/serializers/placement_serializer.rb
@@ -4,6 +4,11 @@ class PlacementSerializer < ActiveModel::Serializer
   attributes :id, :placement_id, :floor_price, :creative
 
   def creative
-    ActiveModelSerializers::SerializableResource.new(object.creative, serializer: CreativeSerializer)
+    return unless object.creative
+
+    ActiveModelSerializers::SerializableResource.new(
+      object.creative,
+      serializer: CreativeSerializer
+    )
   end
 end
--- spec/requests/placements_spec.rb
+++ spec/requests/placements_spec.rb
@@ -11,38 +11,49 @@ RSpec.describe "placements", type: :request do
       )
     end
 
-    let!(:creative1) do
-      Creative.create(
-        creative_id: "Video-1",
-        price: 12
-      )
+    context "when placement exist" do
+      let!(:creative1) do
+        Creative.create(
+          creative_id: "Video-1",
+          price: 12
+        )
+      end
+
+      let!(:creative2) do
+        Creative.create(
+          creative_id: "Video-2",
+          price: 13
+        )
+      end
+
+      it "returns corresponding placement" do
+        get "/placements/#{placement.placement_id}"
+        expect(json["placement_id"]).to eq(placement.placement_id)
+      end
+
+      it "returns placement with creative_2" do
+        get "/placements/#{placement.placement_id}"
+        expect(json["creative"]["creative_id"]).to eq(creative2.creative_id)
+      end
+
+      it "returns placement without creative_1" do
+        get "/placements/#{placement.placement_id}"
+        expect(json["creative"]["creative_id"]).not_to eq(creative1.creative_id)
+      end
     end
 
-    let!(:creative2) do
-      Creative.create(
-        creative_id: "Video-2",
-        price: 13
-      )
+    context "when placment has no creative" do
+      it "returns corresponding placement without creative" do
+        get "/placements/#{placement.placement_id}"
+        expect(json["creative"]).to be_nil
+      end
     end
 
-    it "returns corresponding placement" do
-      get "/placements/#{placement.placement_id}"
-      expect(json["placement_id"]).to eq(placement.placement_id)
-    end
-
-    it "returns placement without creative_1" do
-      get "/placements/#{placement.placement_id}"
-      expect(json["creative"]["creative_id"]).not_to eq(creative1.creative_id)
-    end
-
-    it "returns placement with creative_2" do
-      get "/placements/#{placement.placement_id}"
-      expect(json["creative"]["creative_id"]).to eq(creative2.creative_id)
-    end
-
-    it "returns failure" do
-      get "/placements/test123"
-      expect(json["success"]).to be(false)
+    context "when placement does not exist" do
+      it "returns failure" do
+        get "/placements/test123"
+        expect(json["success"]).to be(false)
+      end
     end
   end
 end
